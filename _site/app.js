f0d68cb689da9a44b740ba117f7a498649c71e2b
Ben Noble <benjamin4504@gmail.com>
2022-07-25 21:33:54 -0400

    remove button on main page

--- _site/app.js
+++ _site/app.js
@@ -8,15 +8,3 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click', mobileMenu);
-
-// const menu = document.querySelector('#mobile-menu');
-// const menuLinks = document.querySelector('.navbar-menu');
-// const navLogo = document.querySelector('#navbar-logo');
-
-// // Display Mobile Menu
-// const mobileMenu = () => {
-//     menu.classList.toggle('is-active');
-//     menuLinks.classList.toggle('active');
-// };
-
-// menu.addEventListener('click', mobileMenu);
