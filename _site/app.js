92940ed3e2bf80c24214fcf50fbd8d61c092fa27
Ben Noble <benjamin4504@gmail.com>
2022-07-25 20:29:57 -0400

    attempts fix for hamburger transition

---
+++ _site/app.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+    menu.classList.toggle('.is-active');
+    menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// const menu = document.querySelector('#mobile-menu');
+// const menuLinks = document.querySelector('.navbar-menu');
+// const navLogo = document.querySelector('#navbar-logo');
+
+// // Display Mobile Menu
+// const mobileMenu = () => {
+//     menu.classList.toggle('is-active');
+//     menuLinks.classList.toggle('active');
+// };
+
+// menu.addEventListener('click', mobileMenu);
